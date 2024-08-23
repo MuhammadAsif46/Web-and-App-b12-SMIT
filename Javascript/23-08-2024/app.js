628991b242f00ac077337fd92c5993437954af53
muhammadasif46 <asifahmed32c@gmail.com>
2024-08-23 12:14:02 +0500

    today JS class friday

---
+++ Javascript/23-08-2024/app.js
@@ -0,0 +1,43 @@
+// alert("hello");
+// alert("world")
+
+// var firstname = "asif"
+// firstname = "ahmed"
+
+
+
+// var city = "karachi"
+
+// alert(city)
+
+// city = "Lahore"
+
+// alert(city)
+
+
+// var username;
+// username = "asif"
+// alert(username)
+// var country = "pakistan"
+// var name = "asif"
+
+// alert(firstname)
+
+
+
+
+// Assignment 01:
+
+// question no : 01.
+
+// alert("hello, Sir")
+
+// question no : 02.
+
+
+// Assignment 01:
+
+alert("hello \n world")
+
+
+
