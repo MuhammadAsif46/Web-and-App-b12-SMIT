8d130e21bccebc76db4eac85ab4fe6bbe7209fa6
muhammadasif46 <asifahmed32c@gmail.com>
2024-08-25 11:36:35 +0500

    today class sunday chapter 3 to 7

---
+++ Javascript/25-08-2024/app.js
@@ -0,0 +1,129 @@
+// alert("welcome!");
+
+
+// console.log("hello");
+// console.log("smit");
+
+// var num = 123;
+
+// var username = "hello"
+// console.log(username); 
+
+// var username = "world"
+// console.log(username);
+
+// var username = "user"
+// console.log(username);
+
+
+// document.write("<h1> asif <br/> ahmed </h1>")
+
+// var a;
+// console.log(a);
+// a =  "smit"
+// console.log(a);
+
+// console.log(abc);
+
+// var saylaniMassItTraining = ""
+// var saylaniWelfareTrust = ""
+
+// var num = "12"
+// var num = 12
+// // console.log(typeof num ,  " ",  num);
+
+// num + 
+// alert(num)
+// console.log(num + 3);
+
+// var num1 = "13"
+
+// var num2 = num1 + 7
+// console.log(num2);
+
+// var num1 = 13
+// var num2 = 12
+// var total = num1 + num2
+// console.log(total);
+
+// var num = 1223
+// var var = "!31"
+
+// var sayalni_mass_it_traing = "fds" //snake-case
+// var $username = "hello"//dollar sign
+// var _username = "hello"//snake-case
+// var userName = "world" //camelCase
+// var saylaniWelfare = "" //camelCase
+
+
+// var num = 2
+// var num1 = 45
+// var total = num - num1
+// console.log(total);
+
+// var num = 2
+// var num1 = 10
+// var total = num1 % num
+// console.log(total);
+
+num++   // post increment
+num--   // post decrement
+++num   // pre increment
+--num   // pre decrement
+
+
+var num = 4
+// num = num + 1
+// num++
+// num--
+// ++num
+// console.log(num++);
+// console.log(num--);
+
+// console.log(--num);
+// console.log(num);
+
+// var num = 2
+// var result = num++ + --num + ++num - num--; 
+            //2    +   //2 +  //3     - //3
+//3 //2 //6 //1 //4 //8 //
+// console.log(result);
+
+// var a = 2
+// var b = 3
+// var res = a++ - b-- + ++a + ++b;
+//         //  2   -  3  +   4  + 3
+// console.log(res);//6 output
+
+var totalCost = (1 + 3) * 4;
+console.log(totalCost);
+
+var resultOfComputation = 2 * (4 * 4) + 2;
+console.log(resultOfComputation);
+
+resultOfComputation = ((2 * 4) * 4) + 2;
+
+resultOfComputation = (2 * 4) * (4 + 2);
+console.log(resultOfComputation);
+                            // 8 * 6 = 48
+                            
+
+
+
+
+
+
+
+// Assignment no 1 (alert)
+
+// question 01
+
+// alert("hello")
+
+// question 02
+
+
+
+
+
+
