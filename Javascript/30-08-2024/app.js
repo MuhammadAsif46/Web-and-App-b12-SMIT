cf87126f406a0d9118eb0baf96b6e52d19277950
muhammadasif46 <asifahmed32c@gmail.com>
2024-08-30 11:24:46 +0500

    today class code friday

---
+++ Javascript/30-08-2024/app.js
@@ -0,0 +1,58 @@
+// var num = 6
+// var total = num++ - --num - num++ + ++num + num--
+//             //6    -    6   -  6    +   8  +   8
+// console.log(total);
+
+
+// var a = 5
+// var b = 4
+// var res = a++ - --a + --b - b++ + --a
+//  //       6/5  - 5  +  3   - 4/3  + 4  //std
+// //          5  -  5  +  3  - 3  +  4  //sir
+// console.log(res);
+
+
+// chap 8 : concatenating text strings
+
+// console.log("hello" + " " + "world");
+
+// var firstName = "Asif"
+// var lastName = "Ahmed"
+// var fullName = firstName + " " + lastName
+// console.log(fullName);
+
+// document.write("<h1>" + fullName + "</h1>")
+
+// console.log("2" + "2");
+// console.log("2 plus 2 equals " + 2 + 2); // 4 //22
+
+// chap 9 : Prompts:
+
+// var userName = prompt("Enter your Name")
+// console.log(userName);
+// alert(userName) 
+
+// var num = +prompt("enter your fav number")
+// console.log(num + 3);
+// console.log(num + 2);
+// console.log(num + 1);
+// console.log(num + 1);
+// console.log(num + 1);
+// console.log(num + 1);
+// console.log(num + 1);
+
+// var userName = prompt("enter your name","Asif")
+// console.log(userName);
+
+// var phone = prompt("enter your name","+923")
+// console.log(phone);
+
+// ?????
+// var num1 = +prompt("Enter your first number?")
+// var opt = prompt("Enter operator?")
+// var num2 = +prompt("Enter your second number?")
+
+// console.log(num1 + opt + num2);
+
+
+
