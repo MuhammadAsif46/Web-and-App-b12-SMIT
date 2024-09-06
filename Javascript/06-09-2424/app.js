31a9ced2d32a7520ad9d85cc202457d892611ab6
muhammadasif46 <asifahmed32c@gmail.com>
2024-09-06 12:10:43 +0500

    today class friday chap 12 to 14

---
+++ Javascript/06-09-2424/app.js
@@ -0,0 +1,84 @@
+// if(true){
+//     console.log("chal raha hai....");
+// }else{
+//     console.log("else chal raha hai....");
+// }
+
+// var lang = prompt("Enter your Language")
+
+// if(lang === "HTML"){
+//     console.log("your are hired");
+// }
+// else{
+//     console.log("you are not hired");
+// }
+
+// var person = "xyz"
+
+// var salary = 100;
+// var bonus = 50;
+
+// var firstTime = 40;
+// var secondTime = 60;
+
+// if (salary + bonus === firstTime + secondTime) { //false
+//     console.log("nahi malegi ");
+// }
+// if (salary + bonus !== firstTime + secondTime) { //true
+//     console.log("mil jayegi");
+// }
+// if (salary + bonus > firstTime + secondTime) { //true
+//     console.log("mil jayegi ");
+// }
+// if (salary + bonus < firstTime + secondTime) { //false
+//     console.log("nahi malegi");
+// }
+// if (salary >= firstTime + secondTime) { //true
+//     console.log("mil jayegi ");
+// }
+// if (salary <= firstTime + secondTime) { //false
+//     console.log("nahi malegi");
+// }
+
+// chap: 14 if statements nested
+
+// var food = prompt("enter your food");
+
+// if (food === "biryani") {
+//     var extraItem = prompt("enter your extraItem")
+//     if(extraItem === "raita"){
+//         console.log("raita bhi leky ajao");
+//     } else if (extraItem === "coldrink") {
+//         console.log("coldrink bhi leky ajao");
+//     } else{
+//         console.log("sirf " + food + " leky ajao");
+//     }
+// } else if (food === "burger") {
+//   console.log("2 leky ajana");
+// } else if (food === "palauo") {
+//   console.log("1/2 leky ajana");
+// } else {
+//   console.log("wapis mat aao");
+// }
+
+
+// chapter : 13 : Testing sets of conditions:
+
+// -> && ----- true && true
+// -> || ----- true || false
+
+
+var weight = 400
+var gender = "male"
+
+// if(weight === 400 && gender === "female"){
+//     console.log("chal raha hai && operator");
+// }else{
+//     console.log("nahi chal raha hai && operator");
+// }
+
+// if(weight === 400 || gender === "female"){
+//     console.log("chal raha hai || operator");
+// }else{
+//     console.log("nahi chal raha hai || operator");
+// }
