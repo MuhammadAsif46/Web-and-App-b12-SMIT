1e4b78fe1e6bd2a4effe25f449a86ce242747949
MUHAMMADASIF46 <asifahmed32c@gmail.com>
2024-09-13 12:19:02 +0500

    today class friday 14 to 17

---
+++ Javascript/13-09-2024/app.js
@@ -0,0 +1,80 @@
+// Nested if practice:
+
+// Required HTML/CSS + bachelor + karachi
+
+// var skillsReq = "HTML/CSS";
+// var eduReq = "bachelor";
+// var cityReq = "karachi";
+
+// var skills = prompt("Enter your Skill");
+// var edu = prompt("Enter your edu");
+// var city = prompt("Enter your city");
+
+// if (skills == skillsReq) {
+//   if (edu == eduReq) {
+//     if (city == cityReq) {
+//       console.log("kal sy ajao...");
+//     } else {
+//       console.log("wahi rahoo....");
+//     }
+//   } else {
+//     console.log("degree leky ajao..");
+//   }
+// } else {
+//   console.log("sikh ky aoo..");
+// }
+
+// ARRAY chapter :- 15
+
+// var students = [1,"ahmed",2,"ali",true]
+// var students = ["asif","ahmed","khan","ali","raza"]
+// // console.log(students);
+// // console.log(students[3])
+// // console.log(students[4],students[1])
+
+// console.log(students.length);
+
+// // students[5] = "zafar"
+// students[students.length] = "zafar"
+// students[students.length] = "aaa"
+// students[students.length] = "bb"
+// // students[6] = "aa"
+// // students[7] = "bb"
+// // students[8] = "cc"
+
+// // students[]
+
+// console.log(students);
+
+// var students = ["asif","ahmed","ali"]
+
+// // pop          // last value delete
+// // push         // last value add
+// // shift        // start value delete
+// // unshift      // start value add
+// // splice       // (index,length)
+// // slice        // (index,length) copy
+
+// // students.pop();
+// students.pop();
+// students.push("raza","khan","hamza")
+// students.shift();
+// students.unshift("waqas","hammad");
+// console.log(students.pop())
+
+// console.log(students);
+
+// var fruits = ["apple", "mango", "orange", "banana"];
+
+// // fruits.splice(3,1)
+// // fruits.splice(3,0,"kiwi","grapes")
+
+// var sliceCopy = fruits.slice(0, 3);
+// console.log(sliceCopy);
+
+// console.log(fruits);
+
+// var arr = ["asif ahmed"]
+// var arr = " asif ahmed ali "
+// console.log(arr.length);
+
