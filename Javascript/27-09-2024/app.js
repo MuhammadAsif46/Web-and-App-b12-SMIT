7aa0345fefcc025b2ae1516587716d7650a3ddfc
muhammadasif46 <asifahmed32c@gmail.com>
2024-09-27 10:29:58 +0500

    today class friday chpter 21 & 22

---
+++ Javascript/27-09-2024/app.js
@@ -0,0 +1,42 @@
+// var str = "HELLO WORLD"
+// var lowerCase = str.toLowerCase()  
+// console.log(lowerCase);
+
+// var str = "hello world"
+// var upperCase = str.toUpperCase()  
+// console.log(upperCase);
+
+// var str = "hello"
+// var cap = str.slice(0,1).toUpperCase() + str.slice(1)
+// console.log(cap); 
+
+// Task one:- hello world 
+
+
+// var small = str.slice(1)
+// var res = cap + small
+// console.log(small);
+ 
+
+
+// var str1 = "hello"
+// var str2 = ["hello","world"]
+// str2[1] = "class"
+// console.log(str2); 
+
+// mutable
+// immutable
+
+
+var str = "helo world user"
+for(var i = 0; i < str.length; i++){
+    // console.log(str[i]);
+    if(str.slice(i, i + 5) === "world"){
+        console.log("found!")
+        // break;
+    }
+}
+
+// task two:- saylani mass it training program
+
+
