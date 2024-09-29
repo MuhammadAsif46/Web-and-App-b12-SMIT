cb30a165933527224e6aec375c66b27b00491c04
muhammadasif46 <asifahmed32c@gmail.com>
2024-09-29 12:34:10 +0500

    today class sunday 23 to 25 chpter

--- Javascript/27-09-2024/app.js
+++ Javascript/27-09-2024/app.js
@@ -1,42 +1,86 @@
 // var str = "HELLO WORLD"
-// var lowerCase = str.toLowerCase()  
+// var lowerCase = str.toLowerCase()
 // console.log(lowerCase);
 
 // var str = "hello world"
-// var upperCase = str.toUpperCase()  
+// var upperCase = str.toUpperCase()
 // console.log(upperCase);
 
 // var str = "hello"
 // var cap = str.slice(0,1).toUpperCase() + str.slice(1)
-// console.log(cap); 
+// console.log(cap);
 
-// Task one:- hello world 
-
+// Task one:- hello world
 
 // var small = str.slice(1)
 // var res = cap + small
 // console.log(small);
- 
-
 
 // var str1 = "hello"
 // var str2 = ["hello","world"]
 // str2[1] = "class"
-// console.log(str2); 
+// console.log(str2);
 
 // mutable
 // immutable
 
-
-var str = "helo world user"
-for(var i = 0; i < str.length; i++){
-    // console.log(str[i]);
-    if(str.slice(i, i + 5) === "world"){
-        console.log("found!")
-        // break;
-    }
-}
+// var str = "helo world user"
+// for(var i = 0; i < str.length; i++){
+//     // console.log(str[i]);
+//     if(str.slice(i, i + 5) === "world"){
+//         console.log("found!")
+//         // break;
+//     }
+// }
 
 // task two:- saylani mass it training program
 
+// for loop
+// ini, condition, increment
 
+// var table = prompt("enter your number")
+// for(var i = 1; i <= 10 ; i++) {
+//     document.write( table + " x " + i + " = " + i * table + "<br/>");
+// }
+
+// var arr = ["hello","world","user","ali"]
+
+// for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+// console.log(cities.length);
+
+// var userCity = prompt("enter your city");
+// var cities = ["karachi", "multan", "lahore", "islamabad"];
+// var flag = false;
+
+// for (var i = 0; i < cities.length; i++) {
+//   if (userCity === cities[i]) {
+//     flag = true;
+//     break;
+//   }
+// }
+
+// if (flag === true) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
+
+// console.log(cities[i]);
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i === 5) {
+//         break;
+//     }
+// }
+
+var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+for (var i = 0; i < firstNames.length; i++) {
+  for (var j = 0; j < lastNames.length; j++) {
+    console.log(firstNames[i] + lastNames[j]);
+  }
+}
---
+++ Javascript/29-09-2024/app.js
@@ -0,0 +1,76 @@
+// var str = "hello world"
+
+// var cap = str.toUpperCase()
+// console.log(cap);
+
+// var str = "HELLO WORLD"
+
+// var cap = str.toLowerCase()
+// console.log(cap);
+
+
+// var str = "hello"
+
+// var firstLeter = str.slice(0,1).toUpperCase() + str.slice(1)
+// console.log(firstLeter);
+
+// var str = "hello"
+// var first = str.slice(0,1).toUpperCase() + str.slice(1,6)
+// var second = str.slice(6,7).toUpperCase() + str.slice(7)
+// console.log(first + second);
+
+
+// var str = prompt("enter text")
+// var first = str.slice(0,1).toUpperCase() + str.slice(1)
+// var words = str.split(" ")
+// var arr = []
+
+// for(var i = 0; i < words.length; i++){
+//     var result = words[i][0].toUpperCase() + words[i].slice(1);
+//     arr.push(result)
+// }
+
+// console.log(arr.join(" "));
+
+
+// var str = "hello"
+// console.log(str[0]);
+// console.log(str[7]);
+
+// console.log(str.slice(0));
+
+// console.log(str.indexOf("e")); //6
+// console.log(str.lastIndexOf("e")); //6
+
+// var text = "To be or not to be.";
+// console.log(segIndex);
+
+// var text = "saylani welfare"
+// var segIndex = text.indexOf("a"); // 1
+// var segIndex1 = text.lastIndexOf("a"); //4
+
+// console.log(segIndex);
+// console.log(segIndex1);
+
+// var str = "abdullah"
+// // console.log(str.charAt(str.length-1));
+// console.log(str.charAt(1));
+// console.log(str.charAt(4));
+
+var str = "hello, i am asif ahmed asif ahmed asif "
+// console.log(str);
+// console.log(str.replace("asif","muhammad"));
+console.log(str.replaceAll("asif","muhammad"));
+
+// task 2: slice
+// hello, i am sayalni mass it training student.
+// task 3: se4
+// hello, i am sayalni mass it training student.
+
+// replace task 4 :
+// hello, i am saylani mass it training student saylani malir campus .
+
+
+
+
+
