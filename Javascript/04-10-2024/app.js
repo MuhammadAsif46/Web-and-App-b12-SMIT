9745128b128b312b25902925bf0e4b9d25e973cf
muhammadasif46 <asifahmed32c@gmail.com>
2024-10-04 11:01:01 +0500

    taday class friday chpter 26 to 30

---
+++ Javascript/04-10-2024/app.js
@@ -0,0 +1,47 @@
+// var num = 2.45
+// var num1 = 2.73
+// console.log(num);
+// console.log(Math.round(num));
+// console.log(Math.round(num1));
+
+// var num = 2.16
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+
+
+// var num = Math.random() * 10
+// console.log(num);
+
+
+// var str = "100"
+// var str2 = "2.9999"
+// // console.log(+str);
+// // console.log(parseInt(str));
+// console.log(parseInt(str2));
+// console.log(parseFloat(str2));
+
+
+
+// var num = 123;
+// console.log(typeof num);
+// console.log(typeof String(num));
+// console.log(typeof String(num) + " " + num.toString());
+// console.log(typeof num.toString());
+
+// var num = Math.random()
+// console.log(typeof num);
+// console.log(typeof Number(num.toFixed(2)));
+
+// var num = Math.random() * 1
+
+
+var bigDecimal = Math.random();
+// console.log(bigDecimal);
+var improvedNum = (bigDecimal * 2) + 1;
+// console.log(improvedNum);
+var numberOfStars = Math.floor(improvedNum);
+// console.log(numberOfStars);
+
+
+
