4832674f1c13effcec9a5f030dd9996da89cffaa
muhammadasif46 <asifahmed32c@gmail.com>
2024-10-13 12:01:30 +0500

    added today sunday class 21 to 35 chapter revison

---
+++ Javascript/13-10-2024/app.js
@@ -0,0 +1,50 @@
+// var str = "smit"
+// console.log(str);
+
+// // str.charAt(2)
+// console.log(str.charAt(2));
+
+
+// var str = "hello, i am smit student. smit and smit"
+
+// console.log(str.replace("smit","SMIT"))
+// console.log(str.replaceAll("smit","SMIT"))
+
+
+// var num = 2.1
+// var num1 = 2.5
+// var num2 = 2.9
+
+// // console.log(Math.round(num));
+// // console.log(Math.round(num1));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num2));
+
+
+
+// var random = (Math.random() * 10) + 1
+// console.log(random);
+
+// var str = "2.34567"
+// // console.log(+str);
+// console.log(parseInt(str));
+// console.log(parseFloat(str));
+
+// console.log(Number(str));
+
+// var numberAsNumber = 1234;
+// var numberAsString = numberAsNumber.toString()
+// console.log(numberAsString);
+
+
+// var total = 12.23456909
+// console.log(total);
+// console.log(total.toFixed(2));
+// console.log(Number(total.toFixed(5)));
+// // var prettyTotal = total.toFixed(2);
+// // console.log(total);
+// // console.log(total);
+
+
+// 12236789780989554
+
