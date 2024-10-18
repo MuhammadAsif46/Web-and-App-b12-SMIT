a545a94849e8b1549c4edf67502d2a3c53bbbd57
muhammadasif46 <asifahmed32c@gmail.com>
2024-10-18 11:31:20 +0500

    today class friday chapter 35 to 40

---
+++ Javascript/18-10-2024/app.js
@@ -0,0 +1,93 @@
+// console.log("hello world");
+
+// function abc(){
+//     console.log("abc function");
+// }
+
+// abc();
+// var xyz = "test"
+
+// function hello(greet,abc){ // parameters
+//     console.log(greet,abc);
+// }
+
+// hello("user",12,"hello",true) // arguments
+// function hello(){ // parameters
+//     console.log(arguments);
+//     console.log(arguments[3]);
+// }
+
+// hello("user",12,"hello",true) // arguments
+
+// function add(a, b){
+//     console.log(a + b);
+// }
+
+// add(2,3)
+// add(6,4)
+
+// function abc(){
+//     var xyz = "world"
+//     return xyz;
+// }
+// var a = abc()
+// console.log(a);
+
+// var xyz = "test"
+
+// var hello = "ali"
+// // var hello = "hello"
+// function abc() {
+//     var hello = "hello"
+//     hello = "smit"
+//     console.log(hello);
+// }
+
+// console.log(hello); // ali
+// abc() // hello -> smit
+
+// console.log(xyz);
+
+// var month = "jan";
+// var day = prompt("enter day");
+// switch (day) {
+//   case "sat":
+//     console.log("saturday");
+//     break;
+//   case "mon":
+//     console.log("monday");
+//     break;
+//   case "fri":
+//     console.log("friday");
+//     break;
+//   case "sun":
+//     console.log("true");
+//     break;
+//   default:
+//     console.log("nothing");
+// }
+
+
+// if (day === "sat" && month === "jan" || hours === ) {
+//   console.log("saturday & jan");
+// } else if (day === "mon") {
+//   console.log("monday");
+// } else if (day === "fri") {
+//   console.log("friday");
+// } else if (day === "sun") {
+//   console.log("true");
+// } else {
+//   console.log("nothing");
+// }
+
+
+// function tellTime() {
+//     var now = new Date();
+//     var theHr = now.getHours();
+//     var theMin = now.getMinutes();
+//     var thesec = now.getSeconds();
+//     document.body.innerHTML = ""
+//     document.write("Current time: "+ theHr + ":" + theMin + ": " + thesec);
+// }
+// // tellTime()
+// setInterval(tellTime,1000)
