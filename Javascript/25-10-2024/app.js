f3e58895dd1381c54fb3af0a752b21ada51be956
muhammadasif46 <asifahmed32c@gmail.com>
2024-10-25 11:57:58 +0500

    today class friday revision 35 to 40 & document topic

---
+++ Javascript/25-10-2024/app.js
@@ -0,0 +1,66 @@
+// function abc(){
+//     console.log("hello");
+// }
+
+// abc()
+// abc()
+// abc()
+// abc()
+
+// function xyz(a,b,c) {
+//   console.log(a,b,c);
+// }
+// // console.log(a);
+
+// xyz("world",2367267,true);
+// // xyz("user");
+
+// function xyz(a) {
+//     var abc = "abc";
+//     var abc1 = "abc1";
+//     if (true) {
+//       console.log("hello");
+//     }
+//     console.log("hbfhsdbh");
+//   return a;
+
+// }
+
+// var res = xyz("world");
+// console.log(res);
+// // xyz("user");
+
+// function minus(a,b) {
+//   return a - b;
+// }
+
+// // console.log(a);
+
+// var res =  minus(2, 3);
+// console.log(res);
+// minus(8, 2);
+// var day = "Sat"
+// var month = "Jan"
+// switch (month) {
+//   case "Sat":
+//     alert("Whoopee");
+//     break;
+//   case "Sun":
+//     alert("Whoopee");
+//     break;
+//   case "Fri":
+//     alert("TGIF!");
+//     break;
+//   default:
+//     console.log("Shoot me now!");
+// }
+function hello(){
+    var name = document.getElementById("name");
+    var email = document.getElementById("email")
+    console.log(name.value);
+    console.log(email.value);
+    name.value = ""
+    email.value = ""
+}
+
+
