115b4065029285f19f6be8efe93b03690b44ea71
muhammadasif46 <asifahmed32c@gmail.com>
2024-10-27 11:47:15 +0500

    added today sunday class chp 40 o 47

---
+++ Javascript/27-10-2024/app.js
@@ -0,0 +1,53 @@
+// console.log("While loop is running");
+
+// var i = 0;
+// while (i <= 3) {
+//   console.log(i);
+//   i++;
+// }
+
+// console.log("do loop is running");
+
+// var i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= -1);
+
+// commenting JS
+// // //
+
+// commenting css
+/* */
+
+// commenting Html
+
+// <!-- -->
+
+/*
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();*/
+
+// console.log("hello");
+
+// function abc(){
+//     console.log("hello");
+// }
+
+// abc();
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+function sum(num) {
+  console.log(num);
+}
+
+// sum(1) //3
+// sum(2) //10
+// sum(3) //10
+// sum(4) //10
+// sum(5) //10
