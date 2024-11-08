af3a32196247be9bb0c78a10b1d604c7609b9998
muhammadasif46 <asifahmed32c@gmail.com>
2024-11-08 12:03:16 +0500

    today class friday chp 51 to 57

---
+++ Javascript/03-11-2024/app.js
@@ -0,0 +1,16 @@
+function submit() {
+  //   var test = document.getElementById("test");
+  //   console.log(test.value);
+
+  //   test.value = "asif";
+
+//   var radio = document.getElementsByName("gender");
+
+//   for (var i = 0; i < radio.length; i++) {
+//     if (radio[i].checked) {
+//       console.log(radio[i].value);
+//     }
+//   }
+}
+
+// console.log(radio[i].checked);
---
+++ Javascript/08-11-2024/app.js
@@ -0,0 +1,59 @@
+// console.log("chal gaya");
+
+// var heading = document.getElementById("heading")
+// // console.log(heading);
+
+// // heading.innerText = "smit class"
+
+// heading.innerHTML = "<span>smit class</span>"
+
+// first 1 way
+// function extendText() {
+//   var longText =
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis.";
+//   var shortText =
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis.";
+//   var para = document.getElementById("para");
+//   var link = document.getElementById("link");
+
+//   if (link.innerHTML === "See more") {
+//     para.innerHTML = longText;
+//     link.innerHTML = "See Less";
+//   } else {
+//     para.innerHTML = shortText;
+//     link.innerHTML = "See more";
+//   }
+// }
+
+// function extendText() {
+//   var longText =
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis.";
+//   // var shortText =
+//   //   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium est distinctio blanditiis.";
+//   var para = document.getElementById("para");
+//   var link = document.getElementById("link");
+//   var large = document.getElementById("large");
+
+//   if (link.innerHTML === "See more") {
+//     large.style.display = "block";
+//     link.innerHTML = "See Less";
+//   } else {
+//     large.style.display = "none";
+//     link.innerHTML = "See more";
+//   }
+// }
+
+// function createPost() {
+//   var input = document.getElementById("postInput");
+//   var main = document.getElementById("main");
+
+//   main.innerHTML = input.value;
+//   input.value = "";
+// }
+
+
+// var main = document.getElementsByTagName("h1");
+// console.log(main[0]);
+// var main = document.getElementById("main")
+// var heading = main.getElementsByTagName("h1")
+// console.log(heading[0].innerHTML);
