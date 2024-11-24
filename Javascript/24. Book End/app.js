80a4009ab6226783a42c6a15eca569fbd1f3b7c4
muhammadasif46 <asifahmed32c@gmail.com>
2024-11-24 13:17:23 +0500

    today class JavaScript es5 complete

---
+++ Javascript/24. Book End/app.js
@@ -0,0 +1,40 @@
+// window.location.assign("http://www.facebook.com");
+// window.location.replace("http://www.facebook.com");
+
+// function reload(){
+//     window.location.reload()
+// }
+
+// var win = window.open("home.html")
+// console.log(win);
+
+// function newScreenClose() {
+//   window.open("home.html")
+// }
+
+// function newScreenClose() {
+//   window.close("home.html");
+// }
+
+// var monkeyWindow = window.open("home.html","win1", "width=600,height=600,left=650,top=200");
+// // console.log("chal rahi hai");
+
+// function submit() {
+//   var select = document.getElementById("select");
+//   if (select.value.length === 0) {
+//     console.log("Please select value");
+//   } else {
+//     console.log(select.value);
+//   }
+// }
+
+try {
+  console.log("hello");
+  //   console.og("hello1");
+  throw "enter correct email";
+  console.log("enter correct email");
+} catch (err) {
+  console.log(err);
+}
+
+alert("home");
