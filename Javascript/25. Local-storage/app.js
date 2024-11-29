12dcca61f95486a8ca5f58d0106890626ca6ab73
muhammadasif46 <asifahmed32c@gmail.com>
2024-11-29 12:15:33 +0500

    added local storage work

---
+++ Javascript/25. Local-storage/app.js
@@ -0,0 +1,35 @@
+// console.log("hello");
+
+// function submit() {
+//   var file = document.getElementById("file");
+//   var image = document.getElementById("image");
+//   image.src = URL.createObjectURL(file.files[0])
+// //   console.log(file.files[0]);
+// }
+
+
+// localStorage.removeItem("email")
+// undefined
+// localStorage.removeItem("name")
+// undefined
+// localStorage.setItem("email","aliraza@gmail.com")
+// undefined
+// localStorage.setItem("name","ali raza")
+// undefined
+// localStorage.setItem("roll no","2")
+// undefined
+// localStorage.clear()
+
+
+// var heading = document.getElementById("heading")
+// var obj = {
+//     name: "jhon"
+// }
+
+// heading.innerHTML = obj.name
+
+
+function submit(){
+    // window.location.href = "home.html"
+    window.open("home.html")
+}
