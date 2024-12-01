87294cf7bd9a5f99f9d36659501e76ffbc0f8298
muhammadasif46 <asifahmed32c@gmail.com>
2024-12-01 14:05:29 +0500

    today revision class local storage & quiz format

---
+++ 3. Javascript/26. revision & quiz format/app.js
@@ -0,0 +1,52 @@
+// console.log("hello");
+
+// localStorage.setItem("name","ali raza")
+// undefined
+// localStorage.setItem("email","ali@gmail.com")
+// undefined
+// localStorage.getItem("name")
+// 'ali raza'
+// localStorage.getItem("email")
+// 'ali@gmail.com'
+// localStorage.removeItem("name")
+// undefined
+// localStorage.removeItem("email")
+// undefined
+// localStorage.setItem("email","ali@gmail.com")
+// undefined
+// localStorage.setItem("name","ali raza")
+// undefined
+// localStorage.clear()
+// undefined
+
+// var time = 0
+
+// var quiz = [
+//     {
+//         question: "abc?",
+//         answers: ["a","b","v","f"],
+//         correctAnswer: "b"
+//     },
+//     {
+//         question: "abc?",
+//         answers: ["a","b","v","f"],
+//         correctAnswer: "b"
+//     },
+// ]
+
+// quiz.innerHtml = `<h1>${}</h1>
+// <br>
+// <input type="radio" class="radio">`
+
+// console.log("hello1");
+
+// setTimeout(() => {
+//     console.log("hello2");
+// }, 5000);
+
+// console.log("hello3");
+
+// setInterval(() => {
+//     console.log("hello2");
+// }, 1000);
+
