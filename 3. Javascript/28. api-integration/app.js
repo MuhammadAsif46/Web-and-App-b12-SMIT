db0f350095042ed8b4c4d12a976d2d31a605b640
muhammadasif46 <asifahmed32c@gmail.com>
2024-12-08 12:26:28 +0500

    add Api-integration using jsonplaceholder dummy api

---
+++ 3. Javascript/28. api-integration/app.js
@@ -0,0 +1,52 @@
+// console.log("hello");
+
+// for(var i = 0; i < 5; i++){
+//     console.log(i);
+// }
+
+// console.log("hello1");
+
+// setTimeout(function () {
+//   console.log("hello1");
+// }, 3000);
+
+// console.log("hello");
+// var obj;
+
+// function foo(callback) {
+//     // console.log(asif);
+//   setTimeout(function () {
+//     var obj = {
+//       name: "ali",
+//       email: "ali@gmail.com",
+//     };
+//     callback(obj)
+//   }, 3000);
+// }
+
+// function abc(data) {
+//   console.log(data.name);
+// }
+// foo(abc)
+
+// setTimeout(function(){
+//     console.log(obj);
+// },4000)
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(function (data) {
+//     return data.json();
+//   })
+//   .then(function (data) {
+//     console.log("data-->",data);
+//   })
+//   .catch(function (err) {
+//     console.log("err-->",err);
+//   });
+
+// callback example:
+// function abc(){
+//     setTimeout(function(){
+//         var xyz = "hello"
+//     })
+// }
