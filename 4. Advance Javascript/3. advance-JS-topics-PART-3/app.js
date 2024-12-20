d15f33ab4954ace810c50db8ad381282ff6912d1
muhammadasif46 <asifahmed32c@gmail.com>
2024-12-20 12:17:13 +0500

    add part -3 advace JS topics

---
+++ 4. Advance Javascript/3. advance-JS-topics-PART-3/app.js
@@ -0,0 +1,112 @@
+
+import { sum } from "./main.js";
+// const {sum} = require("./main");
+// console.log("hello");
+
+// abc()
+// function abc(){
+//     console.log("test");
+// }
+
+// abc()
+
+// var foo = function (){
+//     console.log("hello");
+// }
+
+// foo()
+
+// let abc = (a,b) => a + b;
+
+// console.log(abc(5,2))
+
+// function abc(){
+//     console.log("test");
+// }
+
+// abc()
+// abc.apply()
+// abc.call()
+
+// let abc = () => {
+//     console.log("hello");
+// }
+
+// abc()
+// abc.call()
+
+// class
+
+// class Student {
+//     constructor(name,email){
+//         this.name = name
+//         this.email = email
+//     }
+// }
+
+// let std =  new Student("ali","ali@gmail.com")
+// let std1 =  new Student("ali","ali@gmail.com")
+// console.log(std);
+// console.log(std1);
+
+// inhertoice
+
+// class school {
+//     constructor(schoolName){
+//         this.schoolName = schoolName
+//     }
+// }
+
+// class Student extends school{
+//     constructor(schoolName,name,email){
+//         super(schoolName)
+//         this.name = name
+//         this.email = email
+//     }
+// }
+
+// let std = new Student("abc","ali","ali@mail.com")
+// console.log(std);
+
+// class school {
+//     constructor(schoolName){
+//         this.schoolName = schoolName
+//     }
+//     getName = (schoolName) => {
+//         console.log("My school NAME IS ", schoolName);
+//     }
+// }
+
+// let std = new school("abc")
+// std.getName("abc")
+// console.log(std);
+
+// let stdName = "jhdswju"
+// let age = 123
+
+// let obj = {
+//   stdName,
+//   age,
+//   email: "abc@gmail.com",
+//   greet: function () {
+//     return `welcome! ${this.stdName}`
+//   },
+// };
+
+// console.log(obj.greet())
+
+// let arr = [1, 2, 3];
+
+// for (let abc of arr) {
+//   console.log(abc);
+// }
+
+// import { sum } from "./main";
+
+// console.log(sum(2, 3));
+// return (console.log(sum))
+console.log(sum);
+
+
+
+
