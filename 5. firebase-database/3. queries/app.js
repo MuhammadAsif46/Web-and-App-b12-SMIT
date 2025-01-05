efd2b01fa254e37d2e121bd20d861198b8afad40
muhammadasif46 <asifahmed32c@gmail.com>
2025-01-05 12:33:45 +0500

    add firebase queries

---
+++ 5. firebase-database/3. queries/app.js
@@ -0,0 +1,97 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  limit,
+  where
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCcDba7w8OQqgWBCmje7-0qDWJHESQDJmA",
+  authDomain: "smit-batch-12-bba98.firebaseapp.com",
+  projectId: "smit-batch-12-bba98",
+  storageBucket: "smit-batch-12-bba98.firebasestorage.app",
+  messagingSenderId: "283018929754",
+  appId: "1:283018929754:web:8cc72954132661408e1a63",
+};
+
+// initialization:
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// add post work start here
+let btn = document.getElementById("createPost");
+
+btn.addEventListener("click", async () => {
+  let post = document.getElementById("post");
+  const date = new Date();
+  try {
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "posts"), {
+      post: post.value,
+      time: date.toLocaleString(),
+    });
+    console.log("Document written with ID: ", docRef.id);
+    console.log("document add");
+    post.value = "";
+    getRealTimeData();
+  } catch (error) {
+    console.log(error);
+  }
+});
+// add post work end here
+
+
+
+// get post work start here
+
+
+let data = document.getElementById("main");
+const getRealTimeData = async () => {
+  const allpost = query(
+    collection(db, "posts"),
+    orderBy("time", "desc"),
+    // limit(4),
+    where("school", "==", "smit")
+  );
+
+  data.innerHTML = "";
+  const eachPost = await getDocs(allpost);
+  eachPost.forEach((doc) => {
+    // console.log(doc.id, " => ", doc.data());
+
+    const { post, time } = doc.data();
+    data.innerHTML += `<div class="mt-10"><span>${post}</span> <span> ${time}</span></div>`;
+  });
+};
+getRealTimeData();
+
+// get post work end here
+
+
+
+// delete all post work start here
+
+let deleteBtn = document.getElementById("deletePost");
+
+deleteBtn.addEventListener("click", async () => {
+  const allpost = collection(db, "posts");
+  const eachPost = await getDocs(allpost);
+
+  eachPost.forEach(async (post) => {
+    await deleteDoc(doc(db, "posts", post.id));
+    data.innerHTML = "";
+    console.log("posts", post.id);
+    console.log("delete posts all");
+  });
+});
+
+// delete all post work end here
+
