0df0b3e984a746c669b7bd10da4a1723374c097d
muhammadasif46 <asifahmed32c@gmail.com>
2025-02-09 12:41:32 +0500

    add today class with GSAP-animation basic animation

---
+++ 7. Gsap-Animation/app.js
@@ -0,0 +1,64 @@
+// gsap.to("#box1",{
+//     x:1200,
+//     duration: 2,
+//     delay:1,
+//     rotate: 360,
+// })
+
+// gsap.to("#box2",{
+//     x:1200,
+//     duration: 2,
+//     delay:3,
+// })
+
+// gsap.to("#box3",{
+//     x:1200,
+//     duration: 2,
+//     delay:5,
+//     backgroundColor: "yellow",
+//     borderRadius: "50%"
+// })
+
+
+// gsap.to("#box1",{
+//     x:1200,
+//     duration: 2,
+//     delay:1,
+//     rotate: 360,
+//     repeat:-1
+// })
+
+
+// from()
+
+// gsap.from("#box1",{
+//     x:1200,
+//     duration: 2,
+//     delay:1,
+//     rotate: 360,
+// })
+
+// gsap.from("#box2",{
+//     x:1200,
+//     duration: 2,
+//     delay:3,
+//     backgroundColor: "yellow",
+//     borderRadius: "50%"
+// })
+
+// gsap.from("#box3",{
+//     x:1200,
+//     duration: 2,
+//     delay:5,
+// })
+
+
+gsap.to("#box1",{
+    x:800,
+    y:400,
+    duration: 2,
+    delay:1,
+    rotate: 360,
+    repeat:-1,
+    yoyo:true
+})
