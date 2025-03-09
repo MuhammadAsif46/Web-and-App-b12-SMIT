f8c606c07add2551355e52e78dd24789c51f6d7b
muhammadasif46 <asifahmed32c@gmail.com>
2025-03-09 11:27:44 +0500

    add two class first hellon world react and second components

---
+++ 10. React/02.components-and-styling/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1 className="heading">Hello world</h1>
+      <h1 className="heading">Hello world</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ 10. React/02.components-and-styling/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import "./header.css"
+
+function Header(){
+    return(
+        <div>
+            <h1 className="header">Header</h1>
+        </div>
+    )
+}
+
+export default Header
