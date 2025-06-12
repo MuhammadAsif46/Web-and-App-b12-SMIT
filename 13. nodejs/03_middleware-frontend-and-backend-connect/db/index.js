35f166c341283025b669bf786fc92b7dadc73801
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-06-13 02:20:10 +0500

    update

--- 13. nodejs/03_middleware-frontend-and-backend-connect/db/index.js
+++ 13. nodejs/03_middleware-frontend-and-backend-connect/db/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(
-          "mongodb+srv://<username>:<db-password>@sittech.kitbqlb.mongodb.net/<dbname>?retryWrites=true&w=majority&appName=SmitTech"
+          ""
         );
         console.log(` MongoDB connected `);
         
