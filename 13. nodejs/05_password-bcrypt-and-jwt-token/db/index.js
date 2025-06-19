78ab4c90c1d752c80bea23cbf421499139febf85
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-06-19 10:16:12 +0500

    updade

--- 13. nodejs/05_password-bcrypt-and-jwt-token/db/index.js
+++ 13. nodejs/05_password-bcrypt-and-jwt-token/db/index.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(
-            "mongodb+srv://smit:smit@smittech.kitbqlb.mongodb.net/test?retryWrites=true&w=majority&appName=SmitTech"
+            ""
         );
         console.log(chalk.bgGreen(`MongoDB connected `));
 
